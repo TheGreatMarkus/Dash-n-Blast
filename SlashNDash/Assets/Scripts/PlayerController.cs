2403b6fc8b93732e76f96e27d27513ce9a9020cc
Cristian Aldea <a.crist8@live.com>
2018-02-06 16:01:11 -0500

    Added simple sprites of the player and ground and coded some basic controls

---
+++ SlashNDash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float jumpPower;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void FixedUpdate() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.A)) {
+            rb.AddForce(Vector2.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.AddForce(Vector2.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        
+        }
+    }
+}
