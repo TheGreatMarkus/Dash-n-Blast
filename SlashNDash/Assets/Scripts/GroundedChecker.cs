417766dd56ca405a8d588c2400ae42ffd61083b9
Cristian <a.crist8@live.com>
2018-02-08 22:57:57 -0500

    Implemented working mouvement and jumping. Also cleaned up some misc stuff.

---
+++ SlashNDash/Assets/Scripts/GroundedChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedChecker : MonoBehaviour {
+
+    private PlayerController pc;
+    // Use this for initialization
+    void Start () {
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        pc.setIsGrounded(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        pc.setIsGrounded(false);
+    }
+}
--- SlashNDash/Assets/Scripts/PlayerController.cs
+++ SlashNDash/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed;
-    public float jumpPower;
+
+
+    public float xAcc;
+    public float xSlowdown;
+    public float maxXSpeed;
+    public float initJumpVelocity;
+
+    private bool inputJump;
+    private bool inputRight;
+    private bool inputLeft;
+    private bool isGrounded;
+
     // Use this for initialization
     void Start() {
-
+        inputJump = false;
+        inputLeft = false;
+        inputRight = false;
     }
 
     // Update is called once per frame
     void Update() {
 
+        getInput();
+
+    }
+
+    private void getInput() {
+        //Getting input from the user.
+        if (Input.GetKey(KeyCode.A)) {
+            inputLeft = true;
+        } else {
+            inputLeft = false;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputRight = true;
+        } else {
+            inputRight = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            inputJump = true;
+        }
     }
 
     private void FixedUpdate() {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (Input.GetKey(KeyCode.A)) {
-            rb.AddForce(Vector2.left * speed);
+        // Applying acceleration if the player gives input.
+        if (inputLeft && rb.velocity.x > -maxXSpeed) {
+            rb.AddForce(Vector2.left * xAcc);
         }
-        if (Input.GetKey(KeyCode.D)) {
-            rb.AddForce(Vector2.right * speed);
+        if (inputRight && rb.velocity.x < maxXSpeed) {
+            rb.AddForce(Vector2.right * xAcc);
         }
-        if (Input.GetKeyDown(KeyCode.W)) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-        
+
+        //Slowing the player down if they dont give input.
+
+        if (!inputLeft && rb.velocity.x < 0) {
+            rb.AddForce(Vector2.right * xSlowdown);
         }
+        if (!inputRight && rb.velocity.x > 0) {
+            rb.AddForce(Vector2.left * xSlowdown);
+        }
+
+        if (!inputRight && !inputLeft && Math.Abs(rb.velocity.x) < 0.2) {
+            Vector3 v = rb.velocity;
+            v.x = 0f;
+            rb.velocity = v;
+        }
+
+
+        if (inputJump) {
+            if (isGrounded) {
+                rb.velocity = new Vector2(rb.velocity.x, initJumpVelocity);
+            }
+            inputJump = false;
+        }
+        Debug.Log(rb.velocity);
+    }
+
+    public void setIsGrounded(bool isGrounded) {
+        this.isGrounded = isGrounded;
     }
 }
