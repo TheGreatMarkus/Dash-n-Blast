99bda5abda25857c3ac5ca731c2b3f67b3a7359c
Cristian Aldea <a.crist8@live.com>
2018-02-12 15:43:18 -0500

    Implemented more mouvement features
    The camera now simply follows the player and the player can dash (but only in one direction for now).

---
+++ SlashNDash/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+
+	void Update () {
+        //For now, the camera will just simply follow the player.
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+	}
+}
--- SlashNDash/Assets/Scripts/PlayerController.cs
+++ SlashNDash/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-
     public float xAcc;
     public float xSlowdown;
     public float maxXSpeed;
     public float initJumpVelocity;
     public float initWallJumpSpeed;
+    public float dashSpeed;
+    public float totalDashTime;
 
     private bool inputJump;
     private bool inputRight;
     private bool inputLeft;
+    private bool inputDash;
+
     private bool isGrounded;
     private bool wallLeft;
     private bool wallRight;
+    private bool dashing;
+
+    private float dashTime;
 
     Rigidbody2D rb;
 
     // Use this for initialization
     void Start() {
         inputJump = false;
-        inputLeft = false;
         inputRight = false;
+        inputLeft = false;
+        inputDash = false;
+
         isGrounded = false;
+        wallLeft = false;
+        wallRight = false;
+
+        dashTime = float.MaxValue;
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update() {
-
         getInput();
-
     }
 
     private void getInput() {
@@ -52,10 +63,52 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.W)) {
             inputJump = true;
         }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            inputDash = true;
+        }
     }
 
     private void FixedUpdate() {
 
+        handleDashing();
+        if(dashTime> totalDashTime) {// Not dashing.
+            handleHorizontalMouvement();
+            handleJumping();
+        }
+        
+    }
+
+    private void handleDashing() {
+        if (inputDash) {
+            dashTime = 0;
+            inputDash = false;
+        }
+        if (dashTime < totalDashTime) {
+            rb.velocity = new Vector2(1, 0.2f) * dashSpeed * (float)Math.Pow(Math.E, -dashTime);
+            dashTime += Time.deltaTime;
+
+        }
+    }
+
+    private void handleJumping() {
+        if (inputJump) {
+            if (isGrounded) {
+                rb.velocity = new Vector2(rb.velocity.x, initJumpVelocity);
+                Debug.Log("Jumping");
+            }
+            if (wallLeft && !isGrounded) {
+                rb.velocity = new Vector2(1, 1) * initWallJumpSpeed;
+                Debug.Log("Wall Jumping");
+            }
+            if (wallRight && !isGrounded) {
+                rb.velocity = new Vector2(-1, 1) * initWallJumpSpeed;
+                Debug.Log("Wall Jumping");
+            }
+            inputJump = false;
+        }
+    }
+
+    private void handleHorizontalMouvement() {
         // Applying acceleration if the player gives input.
         if (inputLeft && inputRight) {
             //Do nothing
@@ -84,24 +137,6 @@ public class PlayerController : MonoBehaviour {
         if (inputRight == inputLeft && Math.Abs(rb.velocity.x) < 0.2) {
             rb.velocity = new Vector2(0f, rb.velocity.y);
         }
-
-        if (inputJump) {
-            if (isGrounded) {
-                rb.velocity = new Vector2(rb.velocity.x, initJumpVelocity);
-                Debug.Log("Jumping");
-            }
-            if (wallLeft && !isGrounded) {
-                rb.velocity = new Vector2(1, 1) * initWallJumpSpeed;
-                Debug.Log("Wall Jumping");
-            }
-            if (wallRight && !isGrounded) {
-                rb.velocity = new Vector2(-1, 1) * initWallJumpSpeed;
-                Debug.Log("Wall Jumping");
-            }
-            inputJump = false;
-        }
-
-
     }
 
     public void setIsGrounded(bool isGrounded) {
@@ -121,6 +156,7 @@ public class PlayerController : MonoBehaviour {
         + "\ninputJump: " + inputJump
         + "\ninputRight: " + inputRight
         + "\ninputLeft: " + inputLeft
-        + "\nisGrounded: " + isGrounded;
+        + "\nisGrounded: " + isGrounded
+        + "\n Dash Time: " + dashTime;
     }
 }
